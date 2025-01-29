d6d54cc7ce8a107f62947baf4acdb86869da1ed8
Alex Carr <alexcarr1113@outlook.com>
2025-01-29 10:44:05 +0000

    added recursive versions of factorial

---
+++ src/main/java/recursivefunctions/Factorial.java
@@ -0,0 +1,23 @@
+package recursivefunctions;
+
+public class Factorial {
+    private static long fact(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
+    }
+
+    private static long factTR(int n, int a) {
+        if (n <= 1) {
+            return a;
+        }
+
+        return factTR(n - 1, n * a);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factTR(5, 1));
+    }
+}
